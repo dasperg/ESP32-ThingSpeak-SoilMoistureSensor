3bfa8f52bd1e9dbb3bb78ba44a6457813b90f799
dasperg <dasperg@gmail.com>
2021-10-24 22:24:48 +0200

    Example config.h

---
+++ config.h
@@ -0,0 +1,7 @@
+// Wifi credentials
+#define WIFI_SSID "Your_Wifi_Name"
+#define WIFI_PASS "Your_Wifi_Password"
+
+// ThingSpeak credentials
+#define TS_CHANNEL_ID 123456
+#define TS_API_KEY "WRITE_API_KEY"
